618db3cdf35fd8b5bed625844d66f5fad9e9fead
Ahmed Saeed <ahmed.saeed@tenpearls.com>
2020-01-20 12:32:00 +0500

    Added image tag to view image Added text area to copy the base64

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let pictureUrl = "";
-  const [image, setImage] = useState({
-    url: pictureUrl,
+  const [viewImage, setViewImage] = useState("");
+  const [image] = useState({
+    url: viewImage,
     file: new File([], "", {})
   });
 
   const handleFileChange = (e) => {
     console.log(URL.createObjectURL(e.target.files[0]))
     if (e.target.files.length <= 0) return;
-    // setImage({
-    //   url: URL.createObjectURL(e.target.files[0]),
-    //   file: e.target.files[0]
-    // });
-
     var reader = new FileReader();
     reader.readAsDataURL(e.target.files[0]);
     reader.onloadend = function () {
       var base64data = reader.result;
       console.log(base64data);
+      setViewImage(base64data)
     }
-
   }
+
   return (
     <div>
-
       <div className="custom-file">
         <input
           type="file"
@@ -37,14 +31,11 @@ function App() {
           accept="image/png, image/jpeg"
           onChange={handleFileChange} />
         <label className="custom-file-label" >{image.file.name ? image.file.name : 'Choose file'}</label>
+        <div style={{ display: "flex", marginTop: "20px" }} >
+          {viewImage ? <img src={viewImage} alt=""></img> : ''}
+          {viewImage ? <div><h3 style={{ textAlign: "center " }}>Base64</h3><textarea style={{ width: "900px", height: "200px" }}>{viewImage}</textarea></div> : ''}
+        </div>
       </div>
-      {/* 
-      <button
-        label="submit"
-        type="submit"
-      //onClick={submit}
-      >Click Me</button> */}
-
     </div>
   );
 }
